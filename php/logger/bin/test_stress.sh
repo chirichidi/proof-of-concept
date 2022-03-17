13a36dece831daebaf74297d3d9593040494a4f5
chirichidi <tolessnoise@gmail.com>
2022-03-17 19:23:34 +0900

    refactoring configs

--- php/logger/bin/test_stress.sh
+++ php/logger/bin/test_stress.sh
@@ -1,6 +1,5 @@
 docker run \
   --rm \
   --network host \
-  --name dummy \
   -v /data/app/proof-of-concept/php/logger:/opt/project \
   chirichidi/proof-of-concept-php8-jit-runtime php /opt/project/bin/test_stress.php
